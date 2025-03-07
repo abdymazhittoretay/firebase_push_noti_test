291807a8d6c1d0417d5af7cf839a4e5b787d0a02
Toretay <abdymvzhit@gmail.com>
2025-03-07 12:24:34 +0500

    created navigatorKey

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_push_noti_test/api/firebase_api.dart';
 import 'package:firebase_push_noti_test/firebase_options.dart';
 import 'package:firebase_push_noti_test/pages/home_page.dart';
+import 'package:firebase_push_noti_test/pages/notification_page.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -11,11 +12,18 @@ void main() async {
   runApp(const MyApp());
 }
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      navigatorKey: navigatorKey,
+      routes: {"/notification_page": (context) => NotificationPage()},
+    );
   }
 }
